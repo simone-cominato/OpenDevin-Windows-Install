05166ab22032f970a6e29a6e7ed339a81b1e7181
zenitogr <109196423+zenitogr@users.noreply.github.com>
2024-07-30 04:00:26 +0300

    Update run_opendevin.sh

--- run_opendevin.sh
+++ run_opendevin.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env sh
-
-# Use Unix line ending (so don't edit the script with notepad)
-# Call the script with the following command (assuming WSL is installed)
-# wsl -e ./run_opendevin.sh
 # Define variables
 WORKSPACE_BASE=$(pwd)/workspace;
 SANDBOX_USER_ID=$(id -u)
 PERSIST_SANDBOX="true"
-# SSH_PASSWORD="make something up here"
 SSH_PASSWORD="sshpassword"
-IMAGE_NAME="ghcr.io/opendevin/opendevin:0.5"
+IMAGE_NAME="ghcr.io/opendevin/opendevin"
 CONTAINER_PORT=3000
-HOST_PORT=3000
-
+HOST_PORT=3000 
+NAME=opendevin-app-$(date +%Y%m%d%H%M%S)
+echo "$NAME\n" >> "docker_container_names.txt"
 # Run the Docker container
 docker run -it \
-    --pull=missing \
+    --pull=always \
     -e SANDBOX_USER_ID=$SANDBOX_USER_ID \
+    -e LLM_MODEL="openai/<your-model-name-from-lm-studio>" \
+    -e LLM_BASE_URL="http://host.docker.internal:1234/v1" \
+    -e CUSTOM_LLM_PROVIDER="openai" \
     -e PERSIST_SANDBOX=$PERSIST_SANDBOX \
     -e SSH_PASSWORD=$SSH_PASSWORD \
     -e WORKSPACE_MOUNT_PATH=$WORKSPACE_BASE \
@@ -24,4 +23,5 @@ docker run -it \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -p $HOST_PORT:$CONTAINER_PORT \
     --add-host host.docker.internal:host-gateway \
+    --name $NAME \
     $IMAGE_NAME
